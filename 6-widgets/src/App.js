8556b33a8b43a4ce5596979cf20a335777df2c5a
mendezona <joseph_mendezona@hotmail.com>
2022-04-04 18:43:04 +1000

    widgets app

---
+++ 6-widgets/src/App.js
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+import Route from './components/Route';
+import Header from "./components/Header";
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React',
+        content: 'React is a favourite JS library'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use react by creating components'
+    },
+]
+
+const options = [
+    {
+        label: 'The colour of red',
+        value: 'red'
+    },
+    {
+        label: 'The colour of blue',
+        value: 'blue'
+    },
+    {
+        label: 'The colour of green',
+        value: 'green'
+    }
+];
+
+export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
+    return (
+        <div>
+            <Header />
+            <Route path='/'>
+                <Accordion items={items} />
+            </Route>
+            <Route path='/list'>
+                <Search />
+            </Route>
+            <Route path='/dropdown'>
+                <Dropdown
+                    label='Select a color'
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path='/translate'>
+                <Translate />
+            </Route>
+        </div>
+    );
+};
